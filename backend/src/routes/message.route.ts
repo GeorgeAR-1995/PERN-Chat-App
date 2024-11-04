647195964a6fddd9ad71d52876e68f5aeaeb07e9
George Ramsey <georgeramsey@live.com>
2024-11-04 14:33:22 +0000

    message routes added and tested successfully

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import protectRoute from '../middleware/protectRoute.js';
-import { sendMessage, getMessages } from '../controllers/message.controller.js';
+import { sendMessage, getMessages, getUsersForSidebar } from '../controllers/message.controller.js';
 
 const messageRoute = express.Router();
 
-messageRoute.post("/send/:id", protectRoute, sendMessage);
+messageRoute.get("/conversations", protectRoute, getUsersForSidebar);
 messageRoute.get("/:id", protectRoute, getMessages);
+messageRoute.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRoute;
