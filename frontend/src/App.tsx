3837f03347a26ad3e6e564d818f5ddc02c3f4d37
George Ramsey <georgeramsey@live.com>
2024-11-05 11:12:48 +0000

    add gender radio component to signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import Login from "./ui_design/pages/login/login";
+import SignUp from "./ui_design/pages/signup/signup";
 
 function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <SignUp />
     </div>
   );
 };
